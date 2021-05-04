a08c8810d768521620c0aedc8b0bcbd5074efe4a
Pavel <39067541+WinglessSparrow@users.noreply.github.com>
2021-05-04 14:24:21 +0200

    Model, and data fetching

---
+++ Models/Product.cs
@@ -0,0 +1,8 @@
+public class Product
+{
+    public string Name { get; set; }
+    public string Cost { get; set; }
+    public string ShortInfo { get; set; }
+    public string Stars { get; set; }
+    public string Amount { get; set; }
+}
