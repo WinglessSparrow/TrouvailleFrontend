198a16485828275ea9b8c5d84cb540e3cb2c57f4
Pavel <39067541+WinglessSparrow@users.noreply.github.com>
2021-05-04 15:20:28 +0200

    I fixed models, and several classes

--- Models/Product.cs
+++
@@ -1,8 +0,0 @@
-public class Product
-{
-    public string Name { get; set; }
-    public string Cost { get; set; }
-    public string ShortInfo { get; set; }
-    public string Stars { get; set; }
-    public string Amount { get; set; }
-}
---
+++ Shared/Models/Product.cs
@@ -0,0 +1,25 @@
+
+namespace TrouvailleFrontend.Shared.Models
+{
+    public class Product
+    {
+        //Product Id
+        public int Pid { get; set; }
+
+        //Categories Id
+        public int[] Cid { get; set; }
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Price { get; set; }
+        public int Stock { get; set; }
+        public int Rating { get; set; }
+
+        public int MinStock { get; set; }
+        //Tax
+        public int Vat { get; set; }
+        //Image Id
+        public int IId { get; set; }
+    }
+}
+
