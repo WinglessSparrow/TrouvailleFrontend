8629a2ca9f2d1c3e5eb9ddef1bac7401dbca5367
Pavel <39067541+WinglessSparrow@users.noreply.github.com>
2021-05-04 15:25:13 +0200

    ready to merge

--- Shared/Models/Product.cs
+++ Shared/Models/Product.cs
@@ -11,7 +11,7 @@ namespace TrouvailleFrontend.Shared.Models
 
         public string Name { get; set; }
         public string Description { get; set; }
-        public string Price { get; set; }
+        public double Price { get; set; }
         public int Stock { get; set; }
         public int Rating { get; set; }
 
