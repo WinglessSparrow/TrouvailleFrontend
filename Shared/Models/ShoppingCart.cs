112860dfcb96e827ea1153e469df819fae0f09cb
abbaru <37040801+abbaru@users.noreply.github.com>
2021-05-05 12:46:32 +0200

    some shoppingCart things added

---
+++ Shared/Models/ShoppingCart.cs
@@ -0,0 +1,10 @@
+public class ShoppingCartItem {
+
+    public ShoppingCartItem(int pid, int amount) {
+        this.Pid = pid;
+        this.Amount = amount;
+    }
+
+    public int Pid {get; set;}
+    public int Amount{get; set;}
+}
