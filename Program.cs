6a7aefe7680676cc2af27e2af600b7a94cba24e1
Pavel <39067541+WinglessSparrow@users.noreply.github.com>
2021-05-07 11:56:11 +0200

    indexing now has boundries

--- Program.cs
+++ Program.cs
@@ -10,17 +10,14 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using TrouvailleFrontend.Shared.Classes;
 
-namespace TrouvailleFrontend
-{
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
+namespace TrouvailleFrontend {
+    public class Program {
+        public static async Task Main(string[] args) {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-            
+
             builder.Services.AddTransient<IProductIterator, ProductsIterator>();
 
             await builder.Build().RunAsync();
--- Shared/Classes/ProductsIterator.cs
+++ Shared/Classes/ProductsIterator.cs
@@ -5,27 +5,24 @@ using System.Net.Http;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
 
-namespace TrouvailleFrontend.Shared.Classes
-{
-    public class ProductsIterator : IProductIterator
-    {
+namespace TrouvailleFrontend.Shared.Classes {
+    public class ProductsIterator : IProductIterator {
         private Product[] _products;
         private int _index = -1;
         private int _numberProductsPerIteration = 7;
         private HttpClient _http;
 
-        public ProductsIterator(HttpClient http)
-        {
+        public ProductsIterator(HttpClient http) {
             _http = http;
         }
 
-        public async Task<List<Product>> GetNextProductsAsync()
-        {
+        public async Task<List<Product>> GetNextProductsAsync() {
             _products = await _http.GetFromJsonAsync<Product[]>("debugData/Products.json");
 
             _index++;
             if (_index * _numberProductsPerIteration > _products.Length) return null;
 
+
             Console.WriteLine(_index);
 
             int offset = _index * _numberProductsPerIteration;
@@ -33,12 +30,14 @@ namespace TrouvailleFrontend.Shared.Classes
 
             return CopyArrayToList(offset, limit);
         }
-        public async Task<List<Product>> GetPreviousProductsAsync()
-        {
+        public async Task<List<Product>> GetPreviousProductsAsync() {
             _products = await _http.GetFromJsonAsync<Product[]>("debugData/Products.json");
 
             _index--;
-            if (_index < 0) return null;
+            if (_index < 0) {
+                _index = -1;
+                return null;
+            }
 
             Console.WriteLine(_index);
 
@@ -47,8 +46,7 @@ namespace TrouvailleFrontend.Shared.Classes
 
             return CopyArrayToList(offset, limit);
         }
-        public async Task<List<Product>> GetProductIndexedAsync(int index)
-        {
+        public async Task<List<Product>> GetProductIndexedAsync(int index) {
             _products = await _http.GetFromJsonAsync<Product[]>("debugData/Products.json");
 
             if (index < 0 || index >= _products.Length) return null;
@@ -60,24 +58,18 @@ namespace TrouvailleFrontend.Shared.Classes
             return CopyArrayToList(offset, limit);
         }
 
-        public async Task<ProductsNumbers> GetProductNumbersAsync()
-        {
+        public async Task<ProductsNumbers> GetProductNumbersAsync() {
             _products = await _http.GetFromJsonAsync<Product[]>("debugData/Products.json");
             return new ProductsNumbers() { NumberOfProduct = _products.Length, NumberProductsPerIteration = _numberProductsPerIteration };
         }
 
-        private List<Product> CopyArrayToList(int from, int to)
-        {
+        private List<Product> CopyArrayToList(int from, int to) {
             List<Product> products = new List<Product>();
 
-            for (int i = from; i < to; i++)
-            {
-                if (!(i >= _products.Length))
-                {
+            for (int i = from; i < to; i++) {
+                if (!(i >= _products.Length)) {
                     products.Add(_products[i]);
-                }
-                else
-                {
+                } else {
                     break;
                 }
             }
