cbee2744ba3a31061a68adfb562b75d43029f656
Pavel <39067541+WinglessSparrow@users.noreply.github.com>
2021-05-15 02:21:57 +0200

    shaping style

--- Program.cs
+++ Program.cs
@@ -11,12 +11,23 @@ using Microsoft.Extensions.Logging;
 using TrouvailleFrontend.Shared.Classes.Test;
 using TrouvailleFrontend.Shared.Classes.Interfaces;
 using TrouvailleFrontend.Shared.Classes.API;
+using Blazorise;
+using Blazorise.Bootstrap;
+using Blazorise.Icons.FontAwesome;
 
 namespace TrouvailleFrontend {
     public class Program {
         public static async Task Main(string[] args) {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
+
+            builder.Services.AddBlazorise(options => {
+                options.ChangeTextOnKeyPress = true;
+            })
+            .AddBootstrapProviders()
+            .AddFontAwesomeIcons();
+
+            // builder.Services.AddBlazorise().AddBootstrapProviders().AddFontAwesomeIcons();
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddTransient<ILocalStorage, LocalStorage>();
