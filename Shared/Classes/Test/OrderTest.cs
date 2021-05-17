b19cb75312d9bb643841c516d3b9a0b9e5279972
Pavel <39067541+WinglessSparrow@users.noreply.github.com>
2021-05-17 02:18:02 +0200

    ProductPage style done

--- Shared/Classes/Test/OrderTest.cs
+++ Shared/Classes/Test/OrderTest.cs
@@ -1,10 +1,15 @@
 using System.Threading.Tasks;
+using System;
+using TrouvailleFrontend.Shared.Classes.API;
 using TrouvailleFrontend.Shared.Classes.Interfaces;
 using TrouvailleFrontend.Shared.Models;
 
 namespace TrouvailleFrontend.Shared.Classes.Test {
     public class OrderTest : IOrder {
         public async Task<bool> OrderAsync(OrderModel order) {
+            //just here to make the compiler shut up
+            await Task.Run(() => { Console.WriteLine(); });
+            
             return true;
         }
     }
