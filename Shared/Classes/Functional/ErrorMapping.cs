d6e0b41fe29bbac149d5f09a613ac4d50919d5f9
Pavel <39067541+WinglessSparrow@users.noreply.github.com>
2021-06-21 16:58:12 +0200

    fixes, modals

--- Shared/Classes/Functional/ErrorMapping.cs
+++ Shared/Classes/Functional/ErrorMapping.cs
@@ -5,7 +5,7 @@ using System.Net;
 namespace TrouvailleFrontend.Shared.Classes.Functional {
     public class ErrorMapping {
         public static Dictionary<HttpStatusCode, string> ErrorsMapped = new Dictionary<HttpStatusCode, string>(){
-            {HttpStatusCode.Unauthorized, "The password and or Login is incorrect"},
+            {HttpStatusCode.Unauthorized, "The Password and or Login is incorrect"},
             {HttpStatusCode.BadRequest, "The request couldn't be understood by the server"},
             {HttpStatusCode.ServiceUnavailable, "The server is unavailable, please contact the administration"},
             //this one will only be shown when the standart Object is used!
