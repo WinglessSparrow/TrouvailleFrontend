3543cc0a3b2aa9084ff2347c33e6491334c4d3a0
Pavel <39067541+WinglessSparrow@users.noreply.github.com>
2021-06-29 14:16:18 +0200

    I fixed a fucksie wucsie

--- Shared/Classes/Functional/ErrorMapping.cs
+++ Shared/Classes/Functional/ErrorMapping.cs
@@ -5,12 +5,12 @@ using System.Net;
 namespace TrouvailleFrontend.Shared.Classes.Functional {
     public class ErrorMapping {
         public static Dictionary<HttpStatusCode, string> ErrorsMapped = new Dictionary<HttpStatusCode, string>(){
-            {HttpStatusCode.Unauthorized, "The Password and or Login is incorrect"},
-            {HttpStatusCode.BadRequest, "The request couldn't be understood by the server"},
-            {HttpStatusCode.ServiceUnavailable, "The server is unavailable, please contact the administration"},
+            {HttpStatusCode.Unauthorized, "401, The Password and or Login is incorrect"},
+            {HttpStatusCode.BadRequest, "400, The request couldn't be understood by the server"},
+            {HttpStatusCode.ServiceUnavailable, "503, The server is unavailable, please contact the administration"},
             //this one will only be shown when the standart Object is used!
-            {HttpStatusCode.OK, "Somebody forgot to set The Error String"},
-            {HttpStatusCode.NotFound, "404"},
+            {HttpStatusCode.OK, "200, Somebody forgot to set The Error String"},
+            {HttpStatusCode.NotFound, "404, oooff"},
             {HttpStatusCode.Forbidden, "403: Oops, You cannot do that"},
             {HttpStatusCode.Conflict, "409"}
         };
